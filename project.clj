3f4079d2e535f7fc429092125ce282a4f867e122
Tyrence <email1@jkbff.com>
2019-09-14 13:24:29 -0600

    update to clojure 1.10.1

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [stylefruits/gniazdo "1.1.1"]              ; web socket client
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "3.7.0"]]
   :main com.jkbff.ps2.real_time_stats.core
+  :init-ns com.jkbff.ps2.real_time_stats.core
   :repl-options {:init-ns com.jkbff.ps2.real_time_stats.core})
