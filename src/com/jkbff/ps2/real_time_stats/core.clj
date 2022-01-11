06d4ba7c6773757b228e7077181b6ab92b1825b4
Tyrence <email1@jkbff.com>
2022-01-10 23:32:04 -0700

    update startup message

--- src/com/jkbff/ps2/real_time_stats/core.clj
+++ src/com/jkbff/ps2/real_time_stats/core.clj
@@ -179,7 +179,7 @@
     (let [characters          (concat (api/get-characters (config/SUBSCRIBE_CHARACTERS)) (get-outfit-characters (config/SUBSCRIBE_OUTFITS)))
           char-map            (zipmap (map :character-id characters) characters)
           clients             (connect char-map (config/SUBSCRIBE_SERVERS))
-          startup-msg         (str "SACA Stats (v10) has started! Tracking " (count characters) " characters.")
+          startup-msg         (str "SACA Stats (v10) has started! Tracking " (count characters) " characters and " (count (config/SUBSCRIBE_SERVERS)) " servers.")
           untracked-chars     (get-untracked-chars char-map)
           is-connected-future (helper/callback-interval (partial is-connected? 60000) 30000)]
 
